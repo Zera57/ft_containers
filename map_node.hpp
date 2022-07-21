872286f2d5bdd9ab51cf31ed3bac40123ec375f6
Zera57 <svetk.avtonomov@gmail.com>
2022-07-21 20:00:56 +0300

    Maded map node

---
+++ map_node.hpp
@@ -0,0 +1,17 @@
+#ifndef __MAP_NODE_HPP__
+# define __MAP_NODE_HPP__
+
+namespace ft {
+
+    template <typename T>
+    struct map_node
+    {
+        T           *value;
+        map_node    *parent;
+        map_node    *left;
+        map_node    *right;
+    };
+    
+}
+
+#endif
