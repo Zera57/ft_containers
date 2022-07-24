8e691f671e0c8675dc92298b2f2777e05d47a03c
Zera57 <svetk.avtonomov@gmail.com>
2022-07-24 14:35:46 +0300

    Making progress in map and map_iterator

--- map.hpp
+++ map.hpp
@@ -1,17 +1,75 @@
 #ifndef __MAP_HPP__
-#define __MAP_HPP__
+# define __MAP_HPP__
 
 # include "reverse_iterator.hpp"
 
 namespace ft {
-    template<
-        class Key,
-        class T,
-        class Compare = std::less<Key>,
-        class Allocator = std::allocator<std::pair<const Key, T> >
-    >
+	template <
+			typename Key,
+			typename T,
+			typename Compare = std::less<Key>,
+			typename Alloc = std::allocator<T>
+			>
     class map {
+        public:
+            typedef	Key													key_type;
+			typedef	T													mapped_type;
+			typedef	std::pair<const Key, T>								value_type;
+			typedef	std::size_t											size_type;
+			typedef	std::ptrdiff_t										difference_type;
+			typedef	Compare												key_compare;
+			typedef	Alloc												allocator_type;
+			typedef	value_type&											reference;
+			typedef	const value_type&									const_reference;
+			typedef Alloc::pointer										pointer;
+			typedef Alloc::const_pointer                                const_pointer;
+			
+			typedef ft::map_iterator<pointer>							iterator;
+			typedef ft::map_iterator<const_pointer>	                   	const_iterator;
+			typedef std::reverse_iterator<iterator>						reverse_iterator;
+			typedef std::reverse_iterator<const_iterator>				const_reverse_iterator;
 
+        private:
+            class value_compare {
+                    
+                    public :
+
+                        typedef bool		result_type;
+                        typedef value_type	first_argument_type;
+                        typedef value_type	second_argument_type;
+
+                    protected :
+
+                        Compare				_comp;
+
+                    public :
+
+                        value_compare(Compare comp) :
+                            _comp(comp) { }
+
+                        ~value_compare() { }
+
+                        result_type		operator () (const first_argument_type &lhs, const second_argument_type &rhs) const { return _comp(lhs.first, rhs.first); }
+
+            } ;
+
+        public:
+            map() {}
+
+            explicit map( const Compare& comp,
+                        const Allocator& alloc = Allocator() ) {}
+
+            template< class InputIt >
+            map( InputIt first, InputIt last,
+                const Compare& comp = Compare(),
+                const Allocator& alloc = Allocator() ) {}
+
+            map( const map& other ) {}
+
+            std::pair<iterator, bool> insert( const value_type& value );
+
+            template< class InputIt >
+            void insert( InputIt first, InputIt last );
     };
 }
 
--- map_iterator.hpp
+++ map_iterator.hpp
@@ -1,12 +1,39 @@
 #ifndef __MAP_ITERATOR_HPP__
-#define __MAP_ITERATOR_HPP__
+# define __MAP_ITERATOR_HPP__
 
 namespace ft {
 
-    class map_iterator {
-        
-    };
+    template <typename T>
+	class map_iterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+		public:
+			typedef	ft::iterator_traits<T>						iterator_traits;
+			typedef typename iterator_traits::difference_type	difference_type;
+			typedef typename iterator_traits::value_type		value_type;
+			typedef typename iterator_traits::pointer			pointer;
+			typedef typename iterator_traits::reference			reference;
+			typedef typename iterator_traits::iterator_category	iterator_category;
 
+			map_iterator() {}
+
+			map_iterator(const T ptr)
+				: m_ptr(ptr) {}
+
+			template<typename Iter>
+			map_iterator(const map_iterator<Iter> it) 
+				: m_ptr(it.base()) {}
+
+			map_iterator& operator++() {
+				++m_ptr;
+				return *this;
+			}
+            map_iterator& operator--() {
+				--m_ptr;
+				return *this;
+			}
+
+		private:
+			pointer		m_ptr;
+    };
 }
 
 #endif
