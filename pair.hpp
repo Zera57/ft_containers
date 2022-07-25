5f3ee1fb7927c3ef75793dcc58cb39d20b0b2415
Zera57 <svetik.avtonomov@gmail.com>
2022-07-25 23:41:30 +0300

    Fixed pair

--- pair.hpp
+++ pair.hpp
@@ -3,31 +3,31 @@
 
 namespace ft {
 
-    template<
-        class T1,
-        class T2
-    > struct pair {
-        public:
-            typedef T1  first_type;
-            typedef T2  second_type;
+	template<
+		class T1,
+		class T2
+	> struct pair {
+		public:
+			typedef T1  first_type;
+			typedef T2  second_type;
 
-            first_type  first;
-            second_type second;
+			first_type  first;
+			second_type second;
 
-            pair() : first(), second() {}
+			pair() : first(), second() {}
 
-            pair( const first_type& x, const second_type& y ) : first(x), second(y) {}
+			pair( const first_type& x, const second_type& y ) : first(x), second(y) {}
 
-            pair( const pair& p ) : first(p.first), second(p.second) {}
+			pair( const pair& p ) : first(p.first), second(p.second) {}
 
-            template< class U1, class U2 >
-            pair( const pair<U1, U2>& p ) : first(p.second), second(p.first) {}
-    };
+			template< class U1, class U2 >
+			pair( const pair<U1, U2>& p ) : first(p.second), second(p.first) {}
+	};
 
-    template< class T1, class T2 >
-    ft::pair<T1,T2> make_pair( T1 t, T2 u ) {
-        return pair(t, u);
-    }
+	template< class T1, class T2 >
+	ft::pair<T1,T2> make_pair( T1 t, T2 u ) {
+		return pair<T1, T2>(t, u);
+	}
 
 }
 
